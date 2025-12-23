34dd7a03d149e780890c867d4589bfdf7e341298
Dev Raval <devraval777dr@gmail.com>
2025-12-23 21:56:35 +0530

    Add signup page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Login from "./Login";
+import Signup from "./Signup";
 
 function App() {
   
 
   return (
     <>
-    <Login/>
+    <Signup/>
+    {/* <Login/> */}
     </>
   )
 }
---
+++ frontend/src/Signup.jsx
@@ -0,0 +1,65 @@
+import logo from "./assets/Applogo.jpeg"
+import "./Signup.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+export default function Signup(){
+    const [showPass,setShowPass] = useState(false);
+  function togglePassword(){
+    setShowPass(!showPass);
+  }
+    return(
+        
+        <div className="sign-up">
+              <div className="left-signup">
+                  <img src={logo} alt="logo" />
+                   <h1>Smart Travel Guide 🌍</h1>
+                    <p>
+                  Discover famous places, local food, temples, museums, and hidden gems
+                  tailored to your district using AI-powered recommendations.
+                    </p>
+              </div>
+              <div className="right-login">
+        <div className="login-box">
+          <h2>Welcome Back 👋</h2>
+          <p className="subtitle">Login to continue planning your journey</p>
+        <form className="login-form">
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input type="email" className="form-control" id="email" />
+          <div className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input type={showPass?"text":"password"} className="form-control" id="password" />
+        </div>
+
+        <div className="mb-3 form-check">
+          <FontAwesomeIcon icon={showPass?faEyeSlash:faEye} className="icon" onClick={togglePassword}/>
+          <label className="form-check-label" htmlFor="check">
+            Show password
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-primary w-100">
+          Submit
+        </button>
+        <div className="lower-box">
+          <p>Forgot <a href="">Password?</a></p>
+          <p>Don't have an account? <a href="">Sign up</a></p>
+        </div>
+        
+      </form>
+      </div>
+      </div>
+        </div>
+    )
+}
